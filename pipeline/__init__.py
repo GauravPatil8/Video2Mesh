c5deae0249a453873579b1c1e12d98c9b2a2e9fd
GauravPatil8 <patilgauravpradeep@gmail.com>
2026-07-21 00:49:28 +0530

    name change

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,10 +1,10 @@
-from pipeline.frames_loader import extract_frames
+from pipeline.frames_loader import load_frames
 from pipeline.colmap_sfm import run_sfm
 from pipeline.gaussian_splatting import train
 from pipeline.mesh_extraction import run_mesh_extraction
 
 __all__ = [
-    "extract_frames",
+    "load_frames",
     "run_sfm",
     "train",
     "run_mesh_extraction",
